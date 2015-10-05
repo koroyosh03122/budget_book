41eef4f1595b17c8d665b1de8856200487a568ed
koro_yosh <y-itoh03@hotmail.co.jp>
2015-10-05 19:36:05 +0900

    hbs edited

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,23 @@
-var chkFormText = function(text) {
+var handlebars = require('express-handlebars');
+
+function chkFormText(text) {
   return typeof(text);
 };
 
-
 module.exports = function(app, models) {
 
-    // // 1件登録(内容)
-    app.post('/post', function(req, res) {
-      // parseInt
-      var costText = parseInt(req.body.costText, 10);
-      var groupId = parseInt(req.body.groupId, 10);
-      var userId = 3;
-
-      // 空白エラー
-      if (costText && groupId) {
-        // 文字列型以外
-        if (chkFormText(costText) === 'number' && chkFormText(costText) === 'number') {
-          models.costs.create({
-              userId: userId,
-              groupId: groupId,
-              costText: costText
-          }).then(function(result) {
-              res.redirect(301, '/');
-          });
-        }
-      }
-    });
+    // function getArr(groupId) {
+    //   var groupArr = [];
+    //   models.group.findAll().then(function(groups) {
+    //     groups.map(function(group) {
+    //       groupArr.push(group.toJSON());
+    //     });
+    //     // console.log( groupArr[groupId - 1]);
+    //
+    //     return groupArr[groupId - 1];
+    //   });
+    // }
+    // console.log(getArr(1));
 
     // 全件取得 スラッシュのとき
     app.get('/', function(req, res) {
@@ -50,15 +42,23 @@ module.exports = function(app, models) {
           return [_costs, _groups];
         });
       }).spread(function(costs, groups) {
+
         res.render("index", {
           scripts: ["app"],
           costsList: costs,
           groupList: groups
         });
+        // console.log(groups);
+
       });
     });
+
+
+
     // ==========================================
     app.get('/detail/:id', function(req, res) {
+      var id = req.params.id;
+
       models.costs.findOne({
         include: [{
           model: models.users,
@@ -70,8 +70,27 @@ module.exports = function(app, models) {
 
       });
     });
+    // // 1件登録(内容)
+    app.post('/post', function(req, res) {
+      // parseInt
+      var costText = parseInt(req.body.costText, 10);
+      var groupId = parseInt(req.body.groupId, 10);
+      var userId = 3;
 
-
+      // 空白エラー
+      if (costText && groupId) {
+        // 文字列型以外
+        if (chkFormText(costText) === 'number' && chkFormText(costText) === 'number') {
+          models.costs.create({
+            userId: userId,
+            groupId: groupId,
+            costText: costText
+          }).then(function(result) {
+            res.redirect(301, '/');
+          });
+        }
+      }
+    });
   }
   // ==========================================
   // // 1件更新
